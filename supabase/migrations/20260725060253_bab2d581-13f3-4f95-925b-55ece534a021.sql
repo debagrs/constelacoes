358bee5cc358d9fea3c20ac8f9ecdd8321ab3a5b
debagrs <debora.gasparetto@ufsm.br>
2026-08-01 10:03:22 -0300

    Delete supabase directory

--- supabase/migrations/20260725060253_bab2d581-13f3-4f95-925b-55ece534a021.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE public.entities REPLICA IDENTITY FULL;
-ALTER TABLE public.atlas_cards REPLICA IDENTITY FULL;
-ALTER PUBLICATION supabase_realtime ADD TABLE public.entities;
-ALTER PUBLICATION supabase_realtime ADD TABLE public.atlas_cards;
